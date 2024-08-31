04788293abfac358cf65bda892fd27add6bdb2e1
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-31 10:44:18 -0300

    fix(react): fix theme import name

--- packages/react/src/@types/styled.d.ts
+++ packages/react/src/@types/styled.d.ts
@@ -1,7 +1,7 @@
 import 'styled-components'
-import { defaultTheme } from '../styles/theme'
+import { bleu } from '../styles/theme'
 
-type ThemeType = typeof defaultTheme
+type ThemeType = typeof bleu
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
