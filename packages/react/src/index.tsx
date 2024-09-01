bf701ccab4b3cd6e04f2f29c10b3b6626029caf3
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-09-01 14:39:55 -0300

    refactor(react): export missing components
    Include components:
    - Button
    - Divider
    - Link
    - Steps
    - TitleWithSeparator

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,10 @@
 export * from './components/Box'
+export * from './components/Button'
+export * from './components/Divider'
+export * from './components/Link'
+export * from './components/Steps'
 export * from './components/Text'
+export * from './components/TitleWithSeparator'
 export * from './styles/theme'
+
+// TODO: export GlobalStyle
