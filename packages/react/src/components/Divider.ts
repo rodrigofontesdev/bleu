8e0fb768978536453b33cb4c7b785b834e439df5
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-31 21:52:57 -0300

    feat(react): create divider component for the package

---
+++ packages/react/src/components/Divider.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Divider = styled.div`
+  height: 2px;
+  background-color: ${({ theme }) => theme.color.blue[400]};
+  opacity: ${({ theme }) => theme.opacity[15]};
+`
