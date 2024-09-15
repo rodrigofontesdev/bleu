7f82f1adc9c93d70090acfd0aba52049010c71d5
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-09-14 23:57:54 -0300

    refactor(react): change divider component folder structure
    This commit continues the commit 99d94be.

--- packages/react/src/components/Divider.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Divider = styled.div`
-  height: 2px;
-  background-color: ${({ theme }) => theme.color.blue[400]};
-  opacity: ${({ theme }) => theme.opacity[15]};
-`
-
-Divider.displayName = 'Divider'
