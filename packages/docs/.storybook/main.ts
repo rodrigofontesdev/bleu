0ec30eccf54b56de9942eedb2233c76a276ec446
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-09-15 00:07:04 -0300

    build(docs): install Storybook a11y addon as dependency
    Improvements:
    - Change Storybook ci command
    - Add Chromatic ci command

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,20 +1,22 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 import { dirname, join } from 'path'
-import remarkGfm from 'remark-gfm'
 import remarkGemoji from 'remark-gemoji'
+import remarkGfm from 'remark-gfm'
 
 function getAbsolutePath(value: string) {
   return dirname(require.resolve(join(value, 'package.json')))
 }
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/docs/*.mdx', '../src/stories/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-a11y'),
     {
       name: '@storybook/addon-docs',
       options: {
@@ -25,7 +27,6 @@ const config: StorybookConfig = {
         },
       },
     },
-    '@chromatic-com/storybook'
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
