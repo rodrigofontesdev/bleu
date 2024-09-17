4379c66d5c20b6f9ffa1f567c02db6546d2be7cc
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-09-16 22:22:42 -0300

    feat(docs): create button documentation

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@bleukit/react'
+import { Button, Text, theme } from '@bleukit/react'
 import {
   faChevronRight,
   faFloppyDisk,
@@ -13,21 +13,39 @@ export default {
   title: 'Buttons/Button',
   component: Button,
   args: {
-    'aria-label': '',
+    appearance: 'primary',
     size: 'normal',
     iconSize: '2x',
-    disabled: false,
     loading: false,
+    disabled: false,
   },
   argTypes: {
-    icon: {
+    appearance: {
+      options: ['primary', 'success', 'danger', 'warning', 'info'],
       control: {
-        disable: true,
+        type: 'inline-radio',
+      },
+      table: {
+        defaultValue: {
+          summary: 'primary',
+        },
+        type: {
+          summary: 'primary | success | danger | warning | info',
+        },
       },
     },
-    variant: {
+    size: {
+      options: ['small', 'normal', 'large'],
       control: {
-        disable: true,
+        type: 'inline-radio',
+      },
+      table: {
+        defaultValue: {
+          summary: 'normal',
+        },
+        type: {
+          summary: 'small | normal | large',
+        },
       },
     },
     iconSize: {
@@ -52,11 +70,28 @@ export default {
       control: {
         type: 'inline-radio',
       },
+      table: {
+        type: {
+          summary:
+            '2xs | xs | sm | lg | xl | 2xl | 1x | 2x | 3x | 4x | 5x | 6x | 7x | 8x | 9x | 10x',
+        },
+        defaultValue: {
+          summary: '2x',
+        },
+      },
     },
-    size: {
-      options: ['small', 'normal', 'large'],
-      control: {
-        type: 'inline-radio',
+    loading: {
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+      },
+    },
+    disabled: {
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
       },
     },
   },
@@ -64,83 +99,170 @@ export default {
 
 type Story = StoryObj<typeof Button>
 
-export const Primary: Story = {
+export const Default: Story = {
+  render: (args) => (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(5, 1fr)',
+        gap: theme.space[5],
+      }}
+    >
+      <Text style={{ gridColumn: '1 / -1' }} align="center" weight="bold" size="sm">
+        Active state
+      </Text>
+
+      <Button {...args} {...Primary.args} />
+      <Button {...args} {...Success.args} />
+      <Button {...args} {...Danger.args} />
+      <Button {...args} {...Warning.args} />
+      <Button {...args} {...Info.args} />
+
+      <Text style={{ gridColumn: '1 / -1' }} align="center" weight="bold" size="sm">
+        Loading state
+      </Text>
+
+      <Button {...args} {...Primary.args} loading />
+      <Button {...args} {...Success.args} loading />
+      <Button {...args} {...Danger.args} loading />
+      <Button {...args} {...Warning.args} loading />
+      <Button {...args} {...Info.args} loading />
+
+      <Text style={{ gridColumn: '1 / -1' }} align="center" weight="bold" size="sm">
+        Disabled state
+      </Text>
+
+      <Button {...args} {...Primary.args} disabled />
+      <Button {...args} {...Success.args} disabled />
+      <Button {...args} {...Danger.args} disabled />
+      <Button {...args} {...Warning.args} disabled />
+      <Button {...args} {...Info.args} disabled />
+    </div>
+  ),
   args: {
     icon: faChevronRight,
-    variant: 'primary',
+  },
+  argTypes: {
+    icon: {
+      control: { disable: true },
+    },
+    iconSize: {
+      control: { disable: true },
+    },
+    appearance: {
+      control: { disable: true },
+    },
+    size: {
+      control: { disable: true },
+    },
+    loading: {
+      control: { disable: true },
+    },
+    disabled: {
+      control: { disable: true },
+    },
+    as: {
+      control: { disable: true },
+      table: {
+        type: {
+          summary: 'button | a',
+        },
+        defaultValue: {
+          summary: 'button',
+        },
+      },
+    },
+  },
+  tags: ['!dev', '!test'],
+}
+
+export const Primary: Story = {
+  args: {
+    appearance: 'primary',
     'aria-label': 'Próxima etapa',
+    icon: faChevronRight,
   },
 }
 
 export const Success: Story = {
   args: {
-    icon: faFloppyDisk,
-    variant: 'success',
+    appearance: 'success',
     'aria-label': 'Salvar perfil',
+    icon: faFloppyDisk,
   },
 }
 
 export const Danger: Story = {
   args: {
-    icon: faTimes,
-    variant: 'danger',
+    appearance: 'danger',
     'aria-label': 'Cancelar',
+    icon: faTimes,
   },
 }
 
 export const Warning: Story = {
   args: {
-    icon: faKey,
-    variant: 'warning',
+    appearance: 'warning',
     'aria-label': 'Atualizar palavra secreta',
+    icon: faKey,
   },
 }
 
 export const Info: Story = {
   args: {
-    icon: faInfo,
-    variant: 'info',
+    appearance: 'info',
     'aria-label': 'Abrir menu de ajuda',
+    icon: faInfo,
   },
 }
 
 export const Loading: Story = {
   args: {
-    icon: faFloppyDisk,
-    variant: 'success',
-    'aria-label': 'Salvar perfil',
+    ...Success.args,
     loading: true,
   },
 }
 
 export const Disabled: Story = {
   args: {
-    icon: faFloppyDisk,
-    variant: 'success',
-    'aria-label': 'Atualizar perfil',
+    ...Success.args,
     disabled: true,
   },
 }
 
 export const Link: Story = {
   args: {
+    appearance: 'primary',
     as: 'a',
-    href: '#',
+    href: 'https://example.com',
+    'aria-label': 'Voltar para Home',
     role: 'button',
     icon: faHouse,
-    variant: 'primary',
-    'aria-label': 'Voltar para Home',
   },
-  argTypes: {
-    disabled: {
-      control: {
-        disable: true,
-      },
+  parameters: {
+    controls: {
+      exclude: ['disabled'],
     },
-    href: {
-      control: {
-        disable: true,
-      },
-    },
+  },
+}
+
+export const Small: Story = {
+  args: {
+    ...Primary.args,
+    size: 'small',
+  },
+}
+
+export const Normal: Story = {
+  args: {
+    ...Primary.args,
+    size: 'normal',
+  },
+}
+
+export const Large: Story = {
+  args: {
+    ...Primary.args,
+    size: 'large',
   },
 }
