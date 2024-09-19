355c84eba7f99bbcc2601881329178efcca651d2
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-09-19 20:24:59 -0300

    feat(docs): create title with separator documentation

--- packages/docs/src/stories/TitleWithSeparator.stories.tsx
+++ packages/docs/src/stories/TitleWithSeparator.stories.tsx
@@ -4,54 +4,109 @@ import type { Meta, StoryObj } from '@storybook/react'
 export default {
   title: 'Typography/Title With Separator',
   component: TitleWithSeparator,
-  args: {
-    children: 'Fancy Title',
-    separator: 'both',
-  },
   argTypes: {
-    separator: {
-      options: ['both', 'left', 'right'],
+    axis: {
+      options: ['horizontal', 'vertical'],
       control: {
         type: 'inline-radio',
       },
+      table: {
+        type: {
+          summary: 'horizontal | vertical',
+        },
+        defaultValue: {
+          summary: 'horizontal',
+        },
+      },
+    },
+    separator: {
+      options: ['both', 'right', 'left'],
+      control: {
+        type: 'inline-radio',
+      },
+      table: {
+        type: {
+          summary: 'both | right | left',
+        },
+        defaultValue: {
+          summary: 'both',
+        },
+      },
+    },
+    separatorSize: {
+      table: {
+        type: {
+          summary: 'number',
+        },
+        defaultValue: {
+          summary: '100',
+        },
+      },
     },
   },
-  render: ({ children, ...props }) => (
-    <div style={{ width: '480px' }}>
-      <TitleWithSeparator {...props}>
-        <h2>{children}</h2>
-      </TitleWithSeparator>
-    </div>
-  ),
+  decorators: [
+    (Story) => (
+      <div style={{ width: '480px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof TitleWithSeparator>
 
 type Story = StoryObj<typeof TitleWithSeparator>
 
-export const Horizontal: Story = {
+export const Default: Story = {
+  render: ({ children, ...props }) => (
+    <TitleWithSeparator {...props}>
+      <h2>{children}</h2>
+    </TitleWithSeparator>
+  ),
   args: {
-    orientation: 'horizontal',
+    children: 'Awesome Title',
+    axis: 'horizontal',
+    separator: 'both',
   },
   argTypes: {
-    orientation: {
-      options: ['horizontal'],
-      control: {
-        type: 'inline-radio',
+    children: {
+      table: { disable: true },
+    },
+    axis: {
+      control: { disable: true },
+    },
+    separator: {
+      control: { disable: true },
+    },
+    separatorSize: {
+      control: { disable: true },
+    },
+  },
+  tags: ['!dev', '!test'],
+}
+
+export const Horizontal: Story = {
+  render: Default.render,
+  decorators: Default.decorators,
+  args: {
+    ...Default.args,
+    separatorSize: 100,
+  },
+  argTypes: {
+    separatorSize: {
+      if: {
+        arg: 'axis',
+        eq: 'vertical',
       },
     },
   },
 }
 
 export const Vertical: Story = {
+  render: Horizontal.render,
   args: {
-    orientation: 'vertical',
-    size: 100,
+    ...Horizontal.args,
+    axis: 'vertical',
   },
   argTypes: {
-    orientation: {
-      options: ['vertical'],
-      control: {
-        type: 'inline-radio',
-      },
-    },
+    ...Horizontal.argTypes,
   },
 }
